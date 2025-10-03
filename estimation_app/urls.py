2af7bc9b1ca89433265200917f5f9ee3ab2b6c2b
adambrauskas <aba@example.com>
2025-10-03 16:13:39 +0300

    add user creation functionality

--- estimation_app/urls.py
+++ estimation_app/urls.py
@@ -1,6 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import RoomView, SubmitVoteView, HomeView, JoinRoomView, RoomStateView, UserLoginView
+from .views import RoomView, SubmitVoteView, HomeView, JoinRoomView, RoomStateView, UserLoginView, UserCreateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -11,5 +11,6 @@ urlpatterns = [
     path("room/<str:code>/state/", RoomStateView.as_view(), name="room_state"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="/login/"), name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
 
 ]
